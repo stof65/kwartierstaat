940ceb99a0b39552e3bac413589b1fc6d4bea098
Kristof Verschaetse <kristof.verschaetse@komatsu.eu>
2014-04-10 22:08:59 +0200

    second commit from aspire

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -22,10 +22,18 @@ exports.gen3 = function(req, res){
 		i1: {name: 'Kristof Verschaetse', birth: {place: 'Menen', date: '1965-07-07'}},
 		i2: {name: 'Maurits Verschaetse'},
 		i3: {name: 'Nadia Samyn'},
-		i4: {name: 'Rafael Verschaetse'},
+		i4: {name: 'Raphael Gerardus Leonardus Verschaetse'},
 		i5: {name: 'Marie-Madeleine Ide'},
-		i6: {name: 'Cyriel Samyn'},
-		i7: {name: 'Maria Lagae'}		
+		i6: {name: 'Cyriel Camiel Samyn'},
+		i7: {name: 'Maria Theresia Lagae'},
+		i8: {name: 'Odilon Joseph Jules Verschaetse'},
+		i9: {name: 'Cesaria Cornelia De Wilde'},
+		i10: {name: 'Maurits August Ide'},
+		i11: {name: 'Madeleine-Louise Verraes'},
+		i12: {name: 'Petrus Joannes Samyn'},
+		i13: {name: 'Louise Mathilde Van Meenen'},
+		i14: {name: 'Constant Ferdinand Lagae'},
+		i15: {name: 'Maria Ludovica Wastyn'}		
 	}
 	});  
 };
